4624304f95290f6115c5df9cb269b5e191f2d36d
Luca Spezzano <spezzanoluca@gmail.com>
2020-01-03 16:24:22 +0100

    testing

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import "./assets/css/global.css";
-import DefaultLayout from '~/layouts/Default.vue'
-
-
 import "aos/dist/aos.css";
 import "lightbox2/dist/css/lightbox.css";
+import DefaultLayout from '~/layouts/Default.vue'
 // import vueSmoothScroll from 'vue2-smooth-scroll'
 // import VueParticles from 'vue-particles'
 
