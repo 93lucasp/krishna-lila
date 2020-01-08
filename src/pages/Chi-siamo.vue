23e79b32b4022d7cc5ba6d4f9675337517b307b1
Luca Spezzano <spezzanoluca@gmail.com>
2020-01-08 14:58:03 +0100

    changed about us

--- src/pages/Chi-siamo.vue
+++ src/pages/Chi-siamo.vue
@@ -1,29 +1,7 @@
 <template>
   <Layout>
-    <h1 class="text-3xl md:text-4xl font-bold mb-3">About us <i class="far fa-address-card ml-2 text-2xl"></i></h1>
-    <p class="text-lg md:text-xl mb-2 font-light">Our mission is to help <b>Frontend Developers</b> to improve their knowledges. <br> We provide <b>articles, snippets of animations, online events and weekly newsletter to keep you updated</b>. </p>
-    <p class="text-lg md:text-xl mb-12 font-light">Our articles are published on
-        <span class="font-bold">Medium</span>, we write for free and only Medium through the
-        <a
-          href="https://medium.com/creators"
-          target="_blank"
-          rel="noopener noreferrer"
-        >Medium Partner Program</a> allows us to get a remuneration for the commitment. <br>
-        The links of Medium that we provide guarantee everyone <b>free access</b> to all our articles.<br></p>
-    <h1 class="text-3xl md:text-4xl font-bold mb-3">Follow Not Only CSS  <i class="fas fa-share-alt ml-2 text-2xl"></i></h1>
-    <ul class="flex items-center mb-12">
-      <li>
-          <a href="https://www.facebook.com/notOnlyCSS/" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f md:text-3xl"></i></a>
-        </li>
-        <li class="mx-4">
-          <a href="https://twitter.com/NotOnlyCSS" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter md:text-3xl"></i></a>
-        </li>
-        <li>
-          <a href="https://medium.com/notonlycss" target="_blank" rel="noopener noreferrer"><i class="fab fa-medium-m md:text-3xl"></i></a>
-        </li>
-    </ul>
-     <h1 class="text-3xl md:text-4xl font-bold mb-3">Write for us <i class="fas fa-pencil-alt ml-2 text-2xl"></i></h1>
-    <p class="text-lg md:text-xl mb-16 font-light">We are always looking for talended writers, if you would like to write for us just drom us few line <a href="mailto:info@notonlycss.com" class="underline font-bold">here</a> </p>
+    <h1 class="text-3xl md:text-4xl font-bold mb-3">Chi siamo</h1>
+   
   </Layout>
 </template>
 
